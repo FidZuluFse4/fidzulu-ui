62596fedc3c76d3983e949cd51cb1e89160db965
dhruvch1244 <dr3515@srmist.edu.in>
2025-09-24 09:35:25 +0530

    routes fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { provideRouter, Routes } from '@angular/router';
-import { LandingPageComponent } from './component/landing-page/landing-page.component';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { ProductsPageComponent } from './component/product-page/product-page.component';
 
 export const routes: Routes = [
-    {path: "landing", component: LandingPageComponent},
-    {path: "login", component: LoginComponent},
-    {path: "register", component: RegisterComponent}
+  { path: 'landing', component: ProductsPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
